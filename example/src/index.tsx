69014dfed9b087b596da6649d434b5ba90171ff4
Jussi Kinnula <jussi.kinnula@wunderdog.fi>
2018-11-27 17:17:52 +0200

    Another attempt to fix the example + bump version

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import { connect } from 'remote-log-websocket'
 import { HomePage } from './HomePage'
 import { NormalCanvasPage } from './NormalCanvasPage'
